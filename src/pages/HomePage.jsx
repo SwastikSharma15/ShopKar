6e45d27b04e3c6174beb5f182956e4043cb4b3f7
Swastik <sharmaspeedx29@gmail.com>
2025-08-21 15:40:09 +0530

    error page

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,37 +1,14 @@
-import './header.css'
-import './HomePage.css'
+import Header from "../Components/Header";
+import CheckmarkIcon from '../assets/images/icons/checkmark.png';
+import "./HomePage.css";
 
 function HomePage() {
   return (
     <>
-      <div className="header">
-        <div className="left-section">
-          <a href="index.html" className="header-link">
-            <img className="logo" src="images/logo-white.png" />
-            <img className="mobile-logo" src="images/mobile-logo-white.png" />
-          </a>
-        </div>
+      <title>Ecommerce Website</title>
+      <link rel="icon" type="image/svg+xml" href="home-favicon.png" />
 
-        <div className="middle-section">
-          <input className="search-bar" type="text" placeholder="Search" />
-
-          <button className="search-button">
-            <img className="search-icon" src="images/icons/search-icon.png" />
-          </button>
-        </div>
-
-        <div className="right-section">
-          <a className="orders-link header-link" href="orders.html">
-            <span className="orders-text">Orders</span>
-          </a>
-
-          <a className="cart-link header-link" href="checkout.html">
-            <img className="cart-icon" src="images/icons/cart-icon.png" />
-            <div className="cart-quantity">3</div>
-            <div className="cart-text">Cart</div>
-          </a>
-        </div>
-      </div>
+      <Header />
 
       <div className="home-page">
         <div className="products-grid">
@@ -75,7 +52,7 @@ function HomePage() {
             <div className="product-spacer"></div>
 
             <div className="added-to-cart">
-              <img src="images/icons/checkmark.png" />
+              <img src={CheckmarkIcon} />
               Added
             </div>
 
@@ -124,7 +101,7 @@ function HomePage() {
             <div className="product-spacer"></div>
 
             <div className="added-to-cart">
-              <img src="images/icons/checkmark.png" />
+              <img src={CheckmarkIcon} />
               Added
             </div>
 
@@ -173,7 +150,7 @@ function HomePage() {
             <div className="product-spacer"></div>
 
             <div className="added-to-cart">
-              <img src="images/icons/checkmark.png" />
+              <img src={CheckmarkIcon} />
               Added
             </div>
 
