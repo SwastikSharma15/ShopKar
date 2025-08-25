c4eb9b340e187a6a5e804c64f1f37df53b027b23
Swastik <sharmaspeedx29@gmail.com>
2025-08-26 03:35:43 +0530

    header interactive

---
+++ src/Components/Header.jsx
@@ -0,0 +1,51 @@
+import { Link } from 'react-router';
+import CartIcon from '../assets/images/icons/cart-icon.png';
+import SearchIcon from '../assets/images/icons/search-icon.png';
+import LogoWhite from '../assets/images/logo-white.png';
+import MobileLogoWhite from '../assets/images/mobile-logo-white.png';
+import './header.css';
+
+function Header( {cart}) {
+
+  let totalQuantity = 0;
+
+  cart.forEach( (cartItem) => {
+    totalQuantity += cartItem.quantity;
+  });
+
+  return (
+    <div className="header">
+      <div className="left-section">
+        <Link to="/" className="header-link">
+          <img className="logo"
+            src={LogoWhite} />
+          <img className="mobile-logo"
+            src={MobileLogoWhite} />
+        </Link>
+      </div>
+
+      <div className="middle-section">
+        <input className="search-bar" type="text" placeholder="Search" />
+
+        <button className="search-button">
+          <img className="search-icon" src={SearchIcon} />
+        </button>
+      </div>
+
+      <div className="right-section">
+        <Link className="orders-link header-link" to="/orders">
+
+          <span className="orders-text">Orders</span>
+        </Link>
+
+        <Link className="cart-link header-link" to="/checkout">
+          <img className="cart-icon" src={CartIcon} />
+          <div className="cart-quantity">{totalQuantity}</div>
+          <div className="cart-text">Cart</div>
+        </Link>
+      </div>
+    </div>
+  );
+}
+
+export default Header
