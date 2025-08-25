b0360baca0fd99aabe5e72c39cff69dd28b9f192
Swastik <sharmaspeedx29@gmail.com>
2025-08-26 03:33:58 +0530

    money js

--- vite.config.js
+++ vite.config.js
@@ -6,5 +6,13 @@ export default defineConfig({
   plugins: [react()],
   server: {
     open: true,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3000'
+      },
+      '/images': {
+        target: 'http://localhost:3000'
+      }
+    }
   }
 })
