d43544245f850b0aa547888ccf4539b6b867906a
Swastik <sharmaspeedx29@gmail.com>
2025-08-26 03:33:29 +0530

    backend integration

--- src/App.jsx
+++ src/App.jsx
@@ -5,13 +5,19 @@ import CheckoutPage from './pages/CheckoutPage'
 import OrdersPage from './pages/OrdersPage'
 import TrackingPage from "./pages/TrackingPage";
 import { NotFoundPage } from './pages/NotFoundPage';
+import { useEffect, useState } from 'react'
+import axios from 'axios';
 
 function App() {
 
+  const [cart, setCart] = useState([])
+
+  
+
   return (
     <Routes>
-      <Route index element={<HomePage />} />
-      <Route path='checkout' element={<CheckoutPage />} />
+      <Route index element={<HomePage cart={cart} />} />
+      <Route path='checkout' element={<CheckoutPage cart={cart} />} />
       <Route path='orders' element={<OrdersPage />} />
       <Route path='tracking' element={<TrackingPage/>} />
       <Route path="*" element={<NotFoundPage />} />
