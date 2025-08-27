543f29e7e072aa8c3d0b2363856c5e2566cd0682
Swastik <sharmaspeedx29@gmail.com>
2025-08-28 02:57:27 +0530

    order summary

---
+++ src/pages/Home/HomePage.jsx
@@ -0,0 +1,34 @@
+import axios from "axios";
+import Header from "../../Components/Header";
+import "./HomePage.css";
+import { useEffect, useState } from "react";
+import ProductsGrid from "./ProductsGrid";
+
+function HomePage({cart}) {
+
+  const [products, setProducts] = useState([]);
+  
+  useEffect(() => {
+    const getHomeData = async () => {
+      const response  = await axios.get("http://localhost:3000/api/products")
+      setProducts(response.data);  
+    }
+    getHomeData();
+  }, []);
+
+  return (  
+    <>
+      <title>Ecommerce Website</title>
+      <link rel="icon" type="image/svg+xml" href="home-favicon.png" />
+
+      <Header cart={cart}/>
+
+      <div className="home-page">
+        <ProductsGrid products={products}/>
+        
+      </div>
+    </>
+  );
+}
+
+export default HomePage;
