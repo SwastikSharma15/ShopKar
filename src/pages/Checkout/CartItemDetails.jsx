d62150f57883d829b6d9249ae6c4a6222bd604ad
Swastik <sharmaspeedx29@gmail.com>
2025-08-31 16:18:34 +0530

    update and del btn working

--- src/pages/Checkout/CartItemDetails.jsx
+++ src/pages/Checkout/CartItemDetails.jsx
@@ -1,7 +1,29 @@
-import { Fragment } from "react";
+import { Fragment, useState } from "react";
 import { formatMoney } from "../../Utils/Money";
+import axios from "axios";
 
-const CartItemDetails = ({cartItem}) => {
+const CartItemDetails = ({ cartItem, loadCart }) => {
+  const [isEditing, setIsEditing] = useState(false);
+  const [newQuantity, setNewQuantity] = useState(cartItem.quantity);
+
+  const deleteCartItem = async () => {
+    await axios.delete(`/api/cart-items/${cartItem.productId}`);
+    await loadCart();
+  };
+
+  const updateCartItem = async () => {
+    if (!isEditing) {
+      setIsEditing(true);
+      return;
+    }
+
+    await axios.put(`/api/cart-items/${cartItem.productId}`, {
+      quantity: Number(newQuantity),
+    });
+    
+    setIsEditing(false);
+    await loadCart();
+  };
   return (
     <Fragment>
       <img className="product-image" src={cartItem.product.image} />
@@ -13,10 +35,32 @@ const CartItemDetails = ({cartItem}) => {
         <div className="product-quantity">
           <span>
             Quantity:{" "}
-            <span className="quantity-label">{cartItem.quantity}</span>
+            {isEditing ? (
+              <input
+                type="number"
+                min={1}
+                value={newQuantity}
+                onChange={(event) => {
+                  setNewQuantity(event.target.value);
+                }}
+                className="quantity-input"
+              />
+            ) : (
+              <span className="quantity-label">{cartItem.quantity}</span>
+            )}
           </span>
-          <span className="update-quantity-link link-primary">Update</span>
-          <span className="delete-quantity-link link-primary">Delete</span>
+          <span
+            className="update-quantity-link link-primary"
+            onClick={updateCartItem}
+          >
+            {isEditing ? "Save" : "Update"}
+          </span>
+          <span
+            className="delete-quantity-link link-primary"
+            onClick={deleteCartItem}
+          >
+            Delete
+          </span>
         </div>
       </div>
     </Fragment>
