0657c3113ee52873a4453563e2b2a381e680d1c2
Swastik <sharmaspeedx29@gmail.com>
2025-08-31 16:19:36 +0530

    orders page with backend

---
+++ src/pages/Orders/OrdersPage.jsx
@@ -0,0 +1,34 @@
+import Header from "../../Components/Header";
+import OrderGrid from "./OrderGrid";
+import "./OrdersPage.css";
+import axios from "axios";
+import { useState, useEffect, Fragment } from "react";
+
+function Orders({ cart }) {
+  const [orders, setOrders] = useState([]);
+
+  useEffect(() => {
+    const fetchOrdersData = async () => {
+      const response = await axios.get("/api/orders?expand=products");
+      setOrders(response.data);
+    };
+    fetchOrdersData();
+  }, []);
+  return (
+    <>
+      <title>Orders</title>
+      <link rel="icon" type="image/svg+xml" href="orders-favicon.png" />
+
+      <Header cart={cart} />
+
+      <div className="orders-page">
+        <div className="page-title">Your Orders</div>
+
+        <OrderGrid orders={orders} />
+
+      </div>
+    </>
+  );
+}
+
+export default Orders;
