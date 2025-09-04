e5f973647acca18b4420d97324c4150fd769e4d1
Swastik <sharmaspeedx29@gmail.com>
2025-09-04 14:34:53 +0530

    user interactive tests

--- src/pages/Home/Product.jsx
+++ src/pages/Home/Product.jsx
@@ -1,4 +1,4 @@
-import { formatMoney } from "../../Utils/Money";
+import { formatMoney } from "../../Utils/money";
 import CheckmarkIcon from "../../assets/images/icons/checkmark.png";
 import axios from "axios";
 import { useState } from "react";
@@ -29,14 +29,14 @@ const Product = ({ product, loadCart }) => {
   return (
     <div className="product-container">
       <div className="product-image-container">
-        <img className="product-image" src={product.image} />
+        <img className="product-image" data-testid='product-image' src={product.image} />
       </div>
 
       <div className="product-name limit-text-to-2-lines">{product.name}</div>
 
       <div className="product-rating-container">
         <img
-          className="product-rating-stars"
+          className="product-rating-stars" data-testid = 'product-rating-stars-image'
           src={`images/ratings/rating-${product.rating.stars * 10}.png`}
         />
         <div className="product-rating-count link-primary">
@@ -68,7 +68,7 @@ const Product = ({ product, loadCart }) => {
         Added
       </div>
 
-      <button className="add-to-cart-button button-primary" onClick={addToCart}>
+      <button className="add-to-cart-button button-primary" data-testid='addToCart-button' onClick={addToCart}>
         Add to Cart
       </button>
     </div>
